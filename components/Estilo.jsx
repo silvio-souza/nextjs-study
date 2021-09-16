5e98317ed25fbd8c3de1a89572ef371d8de6403b
Silvio Souza <sjblu7@gmail.com>
2021-09-16 11:39:37 -0400

    End basic lessons

---
+++ components/Estilo.jsx
@@ -0,0 +1,12 @@
+export default function Estilo(props) {
+  return (
+    <div>
+      <h1 style={{
+        backgroundColor: props.numero >= 0 ? "#2d2" : "#d22",
+        color: props.color,
+        }} >
+        Texto
+      </h1>
+    </div>
+  )
+}
